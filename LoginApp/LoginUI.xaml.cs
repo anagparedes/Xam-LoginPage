a8358ba63023940c4055e34c98a966f09a94bb13
anagparedes <anagparedesg@gmail.com>
2021-08-16 19:28:45 -0400

    Add app

---
+++ LoginApp/LoginUI.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+       public async void Button_Clicked(object sender, EventArgs e)
+        {
+            //El problema esta en el if....
+
+            if (string.IsNullOrEmpty(txtUsuario.Text) && string.IsNullOrEmpty(txtContrasena.Text))
+            {
+                await DisplayAlert("Advertencia", "Los campos son obligatorios.", "Ok");
+                
+
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", "Hola, " + txtUsuario.Text, "Ok");
+            }
+        }
+    }
+}
